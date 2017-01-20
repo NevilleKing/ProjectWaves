b3d5cd35026faf0887d15ef18a91858d43173ee5
David <david.p.gleed@gmail.com>
2017-01-20 19:50:57 +0000

    Random Spawning
    Spawns blocks in a random order.

---
+++ ProjectWaves/Assets/Scripts/Spawning.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject testObj;
+    GameObject SpawnedtestObj;
+    public float TimerMin;
+    public float TimerMax;
+    float Timer;
+
+    void Start()
+    {
+        Timer = Random.Range(TimerMin, TimerMax);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer--;
+        //Debug.Log (Timer);
+        if (Timer <= 0)
+        {
+            //spawn here
+            Debug.Log("Spawn");
+            SpawnedtestObj = Instantiate(testObj) as GameObject;
+            SpawnedtestObj.transform.position = new Vector2(Random.Range(-8.0f, 8.0f), 12.0f);
+            Timer = Random.Range(TimerMin, TimerMax);
+        }
+
+    }
+}
