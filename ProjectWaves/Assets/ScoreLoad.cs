4c8fb30664ee583cae745cf059341e410475cec9
Dean <chayat@hotmail.com>
2017-01-22 14:37:35 +0000

    Added high score

--- ProjectWaves/Assets/ScoreLoad.cs
+++ ProjectWaves/Assets/ScoreLoad.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreLoad : MonoBehaviour {
-
+    public Text highScore;
+    public int score;
+    public int highScoreInt;
 	// Use this for initialization
-	void Start () {
+	void Update () {
         GetComponent<Text>().text = PlayerPrefs.GetInt("currentScore").ToString();
+        score = PlayerPrefs.GetInt("currentScore");
+        highScoreInt = PlayerPrefs.GetInt("highScore");
+
+        if (score > highScoreInt)
+        {
+            PlayerPrefs.SetInt("highScore", score);
+            PlayerPrefs.Save();
+
+        }
+        highScoreInt = PlayerPrefs.GetInt("highScore");
+        highScore.text = highScoreInt.ToString();
+        PlayerPrefs.Save();
 	}
 }
